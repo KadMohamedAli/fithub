74cdbbc37355608abd0c80072a4b81233aba2c9d
den0un <abdennourgamerdz16@gmail.com>
2022-06-08 16:45:18 +0100

    [+] Can we know you king all 4 pages are done.
    [*] What's left to do:
    - switch to cm/in & kg/lb.
    - configure "none of the above" option on fitness level slider.
    - link everything to the database.

--- app/src/main/java/com/example/fithub/CanWeKnowYouKingActivity3.java
+++ app/src/main/java/com/example/fithub/CanWeKnowYouKingActivity3.java
@@ -2,7 +2,9 @@ package com.example.fithub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CanWeKnowYouKingActivity3 extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CanWeKnowYouKingActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_can_we_know_you_king3);
+
+        Button bouttonContinue_3 = findViewById(R.id.continue_3);
+        bouttonContinue_3.setOnClickListener(view -> {
+            Intent intent=new Intent(getApplicationContext(),CanWeKnowYouKingActivity4.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
